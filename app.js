e0df705a5e5abaebfd84ab7b924e2df6456e312d
Cindy <telcindy59@gmail.com>
2023-03-05 17:30:53 +0100

    ff

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const btnMenu = document.querySelector('.logo-menu');
-const menu = document.querySelector('.liste-nav');
+document.addEventListener('DOMContentLoaded', () => {
 
-btnMenu.addEventListener('click', function(){
+    const btnMenu = document.querySelector('.logo-menu');
+    const menu = document.querySelector('.liste-nav');
+
+    btnMenu.addEventListener('click', () => {
     menu.classList.toggle('active');
-})
+    })
 
-const allLinks = document.querySelectorAll('.item-nav');
+    const allLinks = document.querySelectorAll('.item-nav');
 
-allLinks.forEach(function(item){
+    allLinks.forEach(function(item){
 
-    item.addEventListener('click', function(){
+    item.addEventListener('click', () => {
         menu.classList.toggle('active');
     })
 
+    })
 })
-// const alllinks = pour tous les liens je renvoie une fonction,
+console.log('hello');
